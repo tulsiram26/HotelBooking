e900265398b7252726c64ed1c58cc3d36f512ca0
tulsiram26 <gavdetulsiram82@gmail.com>
2025-09-24 01:32:11 +0530

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -117,9 +117,10 @@ export default Footer;
 
 
 
+
 // git init
 // git add README.md
 // git commit -m "first commit"
 // git branch -M main
-// git remote add origin https://github.com/tulsiram26/Rihan-Hospitality.git
+// git remote add origin https://github.com/tulsiram26/HotelBooking.git
 // git push -u origin main
