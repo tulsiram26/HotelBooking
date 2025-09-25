406bdf27da0318486c82c0621b90aa76116281b4
tulsiram26 <gavdetulsiram82@gmail.com>
2025-09-25 20:28:23 +0530

    owner dashboard upodated

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ import Footer from "./components/Footer";
 import AllRooms from "./pages/AllRooms";
 import RoomDetails from "./pages/RoomDetails";
 import MyBookings from "./pages/MyBookings";
+import HotelReg from "./components/HotelReg";
+import Layout from "./pages/hotelOwner/Layout";
+import Dashboard from "./pages/hotelOwner/Dashboard";
+import ListRoom from "./pages/hotelOwner/ListRoom";
+import AddRooom from "./pages/hotelOwner/AddRooom";
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
@@ -13,12 +18,19 @@ const App = () => {
   return (
     <div>
       {!isOwnerPath && <Navbar />}
+      {/* Uncomment below to render HotelReg if needed */}
+      {/* <HotelReg /> */}
       <div className="min-h-[70vh]">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/rooms" element={<AllRooms />} />
           <Route path="/rooms/:id" element={<RoomDetails />} />
           <Route path="/my-bookings" element={<MyBookings />} />
+          <Route path="/owner" element={<Layout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="list-room" element={<ListRoom />} />
+            <Route path="add-room" element={<AddRooom />}/>
+          </Route>
         </Routes>
       </div>
       <Footer />
